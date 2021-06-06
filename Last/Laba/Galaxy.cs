b0135c71165b3c71cfaa2e5b9ccf0729ddf91e95
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-06-06 22:02:57 +0300

    Main

---
+++ Last/Laba/Galaxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Laba.GalaxyTypes;
+
+namespace Laba
+{
+    [XmlInclude(typeof(EllipticClass))]
+    [XmlInclude(typeof(LensClass))]
+    [XmlInclude(typeof(SpiralClass))]
+    [XmlInclude(typeof(UncorrectClass))]
+    [Serializable]
+    public class Galaxy
+    {
+        public String Name { get; set; }
+        public int Count { get; set; }
+
+        public Galaxy() {}
+
+        public Galaxy(String name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+}
---
+++ Last/Laba/GalaxyTypes/EllipticClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba.GalaxyTypes
+{
+    [Serializable]
+    public class EllipticClass : Elliptic
+    {
+        public String EllipticType { get; set; }
+        public EllipticClass() { }
+        public EllipticClass(String name, int count, String ellipticClass) : base(name, count)
+        {
+            EllipticType = ellipticClass;
+        }
+    }
+}
